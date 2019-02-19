6bc2f81f44ad288a2eca5abf2086632aabcd9e20
Will <master_wu51@hotmail.com>
2019-02-19 13:28:44 +1000

    commit 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,13 @@ import { StaticQuery, graphql } from "gatsby";
 import "./bootstrap.min.css";
 import "./layout.css";
 
-const Layout = ({ children }) => <>{children}</>;
+import Navbar from "./Globals/Navbar";
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+  </>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
