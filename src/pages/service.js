a55594757604a6954be9995698a7276c477bbe0f
Will <master_wu51@hotmail.com>
2019-02-21 13:52:21 +1000

    commit 5

---
+++ src/pages/service.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, graphql } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import BackgroundSection from "../components/Globals/BackgroundSection";
+import Info from "../components/Home/Info";
+
+const ServicePage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="our Service"
+      styleClass="about-background"
+    />
+    <Info />
+  </Layout>
+);
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "about-bg.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
+
+export default AboutPage;
